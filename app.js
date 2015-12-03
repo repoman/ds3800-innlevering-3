00ed3c500c553dff4641fccd93e2d42b0a2e9864
Frode Grimstad Bang <frode@riseup.net>
2015-12-03 14:20:33 +0100

    Basic localStorage list

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 $(document).ready(function(e) {
     $('nav button').on('click', changeSectionClickHandler);
     $('.add-picture-button').on('click', addPictureClickHandler);
+    $(".add-thing-button").on("click", addThing);
+
+    db = localStorage;
+    renderThings();
+    //db.clear();
 });
 
 function changeSectionClickHandler(e) {
@@ -22,4 +27,33 @@ function addPicture(url) {
     var html = '<img src="' + url +'" />';
     $('section.pictures img:last').after(html);
     $('.add-picture-url').val('');
+}
+
+function addThing() {
+  var data = JSON.parse(db.getItem("list"));
+  if (data === null) {
+    data = {};
+  }
+  var thingamabob = $('.thingamabob').val();
+  var id = new Date().getTime();
+  var thing = {
+    id: id,
+    title: thingamabob,
+    added: new Date()
+  };
+  data[id] = thing;
+  db.setItem("list", JSON.stringify(data));
+  renderThings();
+  console.log(db.list);
+}
+
+function renderThings() {
+  var data = JSON.parse(db.getItem("list"));
+  var html = "";
+  for (i in data) {
+    console.log(data[i].title);
+    html = html.concat("<li><p>" + data[i].title + "</p></li>");
+  }
+  console.log(html);
+  $(".stupid-list").html(html);
 }
